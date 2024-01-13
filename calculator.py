cbccd75d156cb6587b691dcc3fdb0d74735478d5
jiin <purplejiin@naver.com>
2024-01-13 22:08:35 +0900

    Revert "Add square function"
    This reverts commit c67078afaf53500f7bc519719fbc5b9941e868f2.

--- calculator.py
+++ calculator.py
@@ -1,15 +1,12 @@
 #기본 계산기
 def add(a, b):
-	return a+b
+	return a+b;
 
 def substract(a, b):
-	return a-b
+	return a-b;
 
 def multiply(a, b):
 	return a*b
 
 def divide_free(a, b):
-	return a/b
-
-def square(a):
-	return a*a
+	return a/b;
